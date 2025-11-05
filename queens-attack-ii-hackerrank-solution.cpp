9b92a9740b9354ef0390d9103c0039afe30f4351
Rishi Tiwari <umesh896073@gmail.com>
2025-11-05 11:25:05 +0530

    Update solution: Queen's Attack II (8-directional DFS with obstacle set)

--- queens-attack-ii-hackerrank-solution.cpp
+++ queens-attack-ii-hackerrank-solution.cpp
@@ -1,4 +1,26 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
