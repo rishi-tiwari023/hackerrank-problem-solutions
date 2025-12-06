fca0f7408dab4071cee721cd0a1a035e49b995a1
Rishi Tiwari <umesh896073@gmail.com>
2025-12-06 16:49:08 +0530

    Add Manasa and Stones solution (h-elite3-nov26)

---
+++ manasa-and-stones-hackerrank-solution.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int t, n , a ,b;
+    cin >> t;
+    while(t--){
+        cin >> n >> a >> b;
+        vector<int> arr;
+        if(a==b) arr.push_back((n-1)*a);
+        else{
+            for(int i=1 ; i<=n ; i++){
+                arr.push_back(((n-i)*a)+((i-1)*b));
+            }
+        }
+            sort(arr.begin(), arr.end());
+            for(int i=0 ; i<arr.size() ;i++) cout << arr[i] << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
